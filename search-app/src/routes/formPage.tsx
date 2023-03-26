2137e9be5810d71458e16357ff0d37d27649ad0c
dairinka <interest4me@gmail.com>
2023-03-26 19:41:22 +0300

    feat: add form page

--- search-app/src/routes/formPage.tsx
+++ search-app/src/routes/formPage.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
+import cardData from '../data/data';
+import { FormCardData } from '../types/type';
+import Form from '../components/form/form';
+import FormCardsBlock from '../components/formCard/formCardsBlock';
 
-// export interface IFormPageProps {
-// }
+type formPageState = { arrayCards: FormCardData[] };
+export default class FormPage extends React.Component<object, formPageState> {
+  constructor(prop: formPageState) {
+    super(prop);
+    this.state = {
+      arrayCards: [],
+    };
+  }
 
-export default class FormPage extends React.Component {
+  handleFormCardData = async (newArray: FormCardData[]) => {
+    await this.setState({ arrayCards: [...newArray] });
+  };
   public render() {
     return (
-      <div>
-        <form>
-          <></>
-        </form>
+      <div className="form-page">
+        <p className="form-description">
+          Fill the form below for recieve information about free apartment
+        </p>
+        <Form data={cardData} showCard={this.handleFormCardData} />
+        {this.state.arrayCards.length ? <FormCardsBlock data={this.state.arrayCards} /> : null}
       </div>
     );
   }
